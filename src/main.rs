e40ffd2ac581ff1174b4c6f09e0e2cf57e675bf4
Salmaan Saeed <salsaeed999@gmail.com>
2022-05-29 16:32:53 -0400

    made routing work for Yew web app

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,116 @@
 use yew::prelude::*;
+use yew_router::prelude::*;
 
-enum Msg {
-    AddOne,
+mod pages;
+
+use pages::home::Home;
+use pages::about::About;
+use pages::page_not_found::PageNotFound;
+use yew::html::Scope;
+
+#[derive(Routable, PartialEq, Clone, Debug)]
+pub enum Route {
+    #[at("/")]
+    Home,
+    #[at("/about")]
+    About,
+    #[not_found]
+    #[at("/404")]
+    NotFound,
 }
 
-struct Model {
-    value: i64,
+pub enum Msg {
+    ToggleNavbar,
 }
 
-impl Component for Model {
+pub struct App {
+    navbar_active: bool,
+}
+impl Component for App {
     type Message = Msg;
     type Properties = ();
 
     fn create(_ctx: &Context<Self>) -> Self {
         Self {
-            value: 0,
+            navbar_active: false,
         }
     }
 
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
-            Msg::AddOne => {
-                self.value += 1;
-                // the value has changed so we need to
-                // re-render for it to appear on the page
+            Msg::ToggleNavbar => {
+                self.navbar_active = !self.navbar_active;
                 true
             }
         }
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
-        // This gives us a compenent's "`Scope`" whihc allows us to send messages, etc to the component.
-        let link = ctx.link();
         html! {
-            <div>
-                <button onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
-                <p>{ self.value }</p>
-            </div>
+            <BrowserRouter>
+                { self.view_nav(ctx.link()) }
+
+                <main>
+                    <Switch<Route> render={Switch::render(switch)} />
+                </main>
+                <footer class="footer">
+                    <div class="content has-text-centered">
+                        { "Copyright Salmaan Saeed 2022 " }
+                    </div>
+                </footer>
+            </BrowserRouter>
+        }
+    }
+}
+impl App {
+    fn view_nav(&self, link: &Scope<Self>) -> Html {
+        let Self { navbar_active, .. } = *self;
+
+        let active_class = if !navbar_active { "is-active" } else { "" };
+
+        html! {
+            <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+                <div class="navbar-brand">
+                    <h1 class="navbar-item is-size-3">{ "Sage the Nerd" }</h1>
+
+                    <button class={classes!("navbar-burger", "burger", active_class)}
+                        aria-label="menu" aria-expanded="false"
+                        onclick={link.callback(|_| Msg::ToggleNavbar)}
+                    >
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </button>
+                </div>
+                <div class={classes!("navbar-menu", active_class)}>
+                    <div class="navbar-start">
+                        <Link<Route> classes={classes!("navbar-item")} to={Route::Home}>
+                            { "Home" }
+                        </Link<Route>>
+                        <Link<Route> classes={classes!("navbar-item")} to={Route::About}>
+                            { "About" }
+                        </Link<Route>>
+                    </div>
+                </div>
+            </nav>
+        }
+    }
+}
+
+fn switch(routes: &Route) -> Html {
+    match routes.clone() {
+        Route::Home => {
+            html! { <Home /> }
+        }Route::About => {
+            html! { <About /> }
+        }
+        Route::NotFound => {
+            html! { <PageNotFound /> }
         }
     }
 }
 
 fn main() {
-    yew::start_app::<Model>();
+    wasm_logger::init(wasm_logger::Config::new(log::Level::Trace));
+    yew::start_app::<App>();
 }
